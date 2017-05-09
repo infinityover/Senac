40dace22f4d387e12e2aafbd497e0e5cf15f3079
infinityover <paulobelfi@hotmail.com>
2017-05-09 12:52:32 -0300

    Incluindo códigos com calculo
    Incluindo códigos com calculo

--- sistema solar.c
+++ sistema solar.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
  
 int main ( void )
 {
-    string iniciodocumento = "\documentclass{beamer}"
+      double 
+      mercurioX = 0, venusX = 0, terraX = 0, marteX = 0, jupiterX = 0, saturnoX = 0, uranoX = 0, netunoX = 0, mercurioY, venusY, terraY, marteY, jupiterY, saturnoY, uranoY, netunoY;
+
+      string iniciodocumento ="\documentclass{beamer}"
                               "\usetheme{default}"
                               "\usepackage{tikz}"
                               "\usepackage{xcolor}"
@@ -19,13 +23,31 @@ int main ( void )
                               "\definecolor{urano} {rgb}{0.4,0.4,0.4}"
                               "\definecolor{netuno} {rgb}{0.003921569,0.411764706,0.788235294}";
 
-     string fimdocumento = "\end{document}";
+      string fimdocumento =   "\end{document}";
+
+      FILE * Output;
+      Output = fopen("tikz.txt", "a");
+
+      for(double i = 0; i < 360 ;i++){
+            sin()
+
+
+      }
+     
                     
-                    
-                    
-                        FILE * Output;
-                        Output = fopen("tikz.txt", "a");
+                        
                         fprintf(Output, "data");
                         fclose(Output);
                         return 0;
 }
+
+
+/*thisform.usAngulo = thisform.usAngulo + .01
+THISFORM.shpTERRA.top =  (180*SIN(DTOR(thisform.usANGULO)))
+THISFORM.shpTERRA.left = (180*COS(DTOR(thisform.usANGULO)))
+      
+Thisform.usANGULOLUA = thisform.usANGULOLUA + .1
+THISFORM.shpLUA.top =  THISFORM.shpTERRA.top+12+(30*SIN(DTOR(thisform.usANGULOLUA)))
+THISFORM.shpLUA.left = THISFORM.shpTERRA.left+4+(30*COS(DTOR(thisform.usANGULOLUA)))
+
+*/
